a36c2a49502bc4b6160436be5d79512ada660c31
Jesse Elliott <jesseelliott@outlook.com>
2026-08-17 14:55:34 -0400

    Add secret message / sound

--- code/game/g_target.c
+++ code/game/g_target.c
@@ -518,7 +518,8 @@ void Secret_MarkFound(void) {
 }
 
 void target_secret_use(gentity_t* self, gentity_t* other, gentity_t* activator) {
-	// todo: play sound
+	G_Sound(self, CHAN_ITEM, G_SoundIndex("sound/misc/secret.wav"));
+	trap_SendServerCommand(other->client->ps.clientNum, va("cp \"%s\"", self->message ? self->message : "You found a secret!"));
 	Secret_MarkFound();
 	G_UseTargets(self, activator);
 	G_FreeEntity(self);
